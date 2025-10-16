73e927df85a2ffd8249914f6bd2e9971d576daa4
LukeTDay <luke.day@gmail.com>
2025-10-16 18:20:43 -0400

    Added the /register endpoint that checks if a username is in use and if not creates pushes that new user to the database and returns that user to the frontend

--- todo-api/app/main.py
+++ todo-api/app/main.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import Session
 from app import models, schemas, auth
 from app.database import SessionLocal, engine, Base
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from schemas import UserCreate
 
 #Creates a table if necessary
-Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+app = FastAPI()
 
 def get_db():
     db = SessionLocal()
@@ -16,10 +17,9 @@ def get_db():
     finally:
         db.close()
 
-app = FastAPI()
-
 @app.post("/login", response_model=schemas.Token)
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == form_data.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
     
@@ -29,6 +29,23 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
     access_token = auth.create_access_token(data={"sub", user.username})
     return {"acccess_token": access_token, "token_type": "bearer"}
 
-@app.post("/register", response_model="Out")
+@app.post("/register", response_model=schemas.userOut)
 def register(user: UserCreate, db: Session = Depends(get_db)):
-    pass
+    desired_username = db.query(models.User).filter(models.User.username == user.username).first()
+    if desired_username != None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Username already in use"
+        )
+    
+    hashed_pw = auth.hash_password(user.password)
+
+    new_user = models.User(
+        username = user.username,
+        hashed_password = hashed_pw
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
